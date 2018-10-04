1f18e8a112e11da64ff130a78b885515ba6f2660
王昊 <yingxs888@126.com>
2018-10-04 16:00:04 +0800

    source (MyBatis):执行流程与SqlSessionFactory创建的源码分析

---
+++ mybatis/MyBatis08_HelloWorld/src/test/java/com/yingxs/mybatis/test/MyBatisTest.java
@@ -0,0 +1,55 @@
+package com.yingxs.mybatis.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.yingxs.mybatis.bean.Employee;
+import com.yingxs.mybatis.dao.EmployeeMapper;
+
+public class MyBatisTest {
+
+	
+	public SqlSessionFactory getSqlSessionFactory() throws IOException {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		return new SqlSessionFactoryBuilder().build(inputStream);
+		
+	}
+	
+	/**
+	 * 1.获取sqlSessionFactory
+	 * 2.获取sqlSession对象
+	 * 3.获取接口的代理对象
+	 * 4.执行增删改查方法
+	 * @throws IOException
+	 */
+	@Test
+	public void test01() throws IOException {
+		
+		//1.获取sqlSessionFactory
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		
+		//2.获取sqlSession对象
+		SqlSession openSession = sqlSessionFactory.openSession();
+		
+		try {
+			//3.获取接口的实现类对象
+			//会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+			
+			Employee employee = mapper.getEmpId(1);
+			System.out.println(mapper.getClass());
+			System.out.println(employee);
+		}finally {
+			openSession.close();
+		}
+		
+	}
+
+}
