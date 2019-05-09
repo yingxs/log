1442c7bd0cd0dabf88d339376db6094636197bfa
王昊 <yingxs888@126.com>
2019-05-09 22:35:13 +0800

    demo :国际化，拦截器

---
+++ freemarker/spring-boot-04-webrestfulcurd/src/main/java/com/yingxs/springboot/config/MyMvcConfig.java
@@ -0,0 +1,49 @@
+package com.yingxs.springboot.config;
+
+import com.yingxs.springboot.component.LoginHandlerInterceptor;
+import com.yingxs.springboot.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        super.addViewControllers(registry);
+        registry.addViewController("/yingxs").setViewName("success");
+    }
+
+
+    @Bean
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter(){
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+            }
+
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                // springboot已经做好了静态资源映射 *.js *.css 静态资源可以正常访问
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html","/","/user/login");
+            }
+        };
+        return adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return  new MyLocaleResolver();
+    }
+
+
+}
