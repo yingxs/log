1182cb85e4b15d737fa3643f344a9946891b07b5
王昊 <yingxs888@126.com>
2019-06-08 21:01:05 +0800

    feat :加密与解密

--- Spring-Security/yingxs-security-browser/src/main/java/com/yingxs/security/browser/MyUserDetailService.java
+++ Spring-Security/yingxs-security-browser/src/main/java/com/yingxs/security/browser/MyUserDetailService.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +17,9 @@ public class MyUserDetailService implements UserDetailsService {
 
 	 private Logger logger = LoggerFactory.getLogger(getClass());
 	
+	 @Autowired
+	 private PasswordEncoder passwordEncoder;
+	 
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -23,12 +27,13 @@ public class MyUserDetailService implements UserDetailsService {
 		logger.info("登录用户名："+username);
 		// 根据用户名查找用户信息
 		// 根据查找到的用户信息判断用户是否被冻结
-		
-		return new User(username,"123456",
+		String password = passwordEncoder.encode("123456");
+		logger.info("数据库密码是：" + password);
+		return new User(username,password,
 				true,// 可用
 				true,// 账户未过期
 				true,// 密码未过期
-				false,// 未被锁定
+				true,// 未被锁定
 				AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
 	}
 
