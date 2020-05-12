7a02c6ac99eb4d34bb4d15a32660739993c4308e
王昊 <yingxs888@126.com>
2020-05-12 19:27:42 +0800

    feat: MP下全局配置

--- MyBatis-Plus/mp01/src/main/java/com/yingxs/mp/beans/Employee.java
+++ MyBatis-Plus/mp01/src/main/java/com/yingxs/mp/beans/Employee.java
@@ -1,5 +1,6 @@
 package com.yingxs.mp.beans;
 
+import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.IdType;
@@ -15,18 +16,22 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@TableName("tbl_employee")
+//@TableName("tbl_employee")
 public class Employee {
 
     /**
      * value 指定表中的主键的列名，如果实体属性名和别名一致，可以省略不指定
      * type  指定主键策略
      */
-    @TableId(type = IdType.AUTO)
+    //@TableId(type = IdType.AUTO)
     private Integer id;
     private String lastName;
     private String email;
     private Integer gender;
     private Integer age;
+
+    // 写入数据库时 忽略该字段
+    @TableField(exist = false)
+    private Double salary;
 
 }
